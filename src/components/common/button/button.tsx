1c5031241cb64366b90a7394d88a80992f63fd66
SenaGust <gustavosena98@live.com>
2025-09-25 15:41:12 -0300

    fixed some layout issues

--- src/components/common/button/button.tsx
+++ src/components/common/button/button.tsx
@@ -7,7 +7,7 @@ export function Button(props: ButtonProps) {
   return (
     <button
       className={twMerge(
-        "min-w-40 px-6 py-4 justify-center box-border",
+        "min-w-40 px-6 py-4 justify-center box-border select-none",
         "bg-surface border border-stroke backdrop-blur-xs rounded-lg",
         "hover:bg-surface-hover hover:shadow-lg hover:border-primary",
         "cursor-pointer text-md text-primary",
