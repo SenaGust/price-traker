d660687c00a4f826c32d8459715d3aed73b490c5
SenaGust <gustavosena98@live.com>
2025-09-25 11:57:58 -0300

    added icon libraries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+import {
+  MoonIcon,
+  SunIcon,
+  AmazonLogoIcon,
+} from "@phosphor-icons/react/dist/ssr";
+import { logoAmazon } from "ionicons/icons";
+import Image from "next/image";
+
 export default async function Home() {
   return (
     <div>
+      <MoonIcon />
+      <SunIcon />
+      <AmazonLogoIcon />
+      <Image src={logoAmazon} alt="" width={32} height={32} />
+
       <span className="text-sm text-primary">text-sm</span>
       <br />
       <span className="text-md text-primary">text-md</span>
